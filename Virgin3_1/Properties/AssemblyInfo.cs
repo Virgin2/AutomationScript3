099bd3bc89bbb5c5a6c2c1e7b87d93e8e1f6ec4d
B5302165 <B5302165@LAP-0104592>
2023-06-13 14:33:14 +0100

    Add project files.

---
+++ Virgin3_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Virgin3_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Virgin3_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("65963D2E-576C-493F-B09A-8314CB1AF75C")]
+[assembly: AssemblyVersion("1.0.0.0")]
